d62990e9a56923b0a01a126c53a568c29e473bf0
alxpez <alexperez.dev@gmail.com>
2023-12-30 13:18:40 +1300

    feat: refactor poc to assistant class: - create Assistant class - refactor logic and function naming - include standalone mode (dev-mode)

---
+++ assistant.py
@@ -0,0 +1,229 @@
+import os
+import sys
+import whisper
+import queue
+import threading
+import tempfile
+import keyboard
+from sounddevice import InputStream, query_devices
+from soundfile import SoundFile
+from TTS.api import TTS # TODO: see other options (pyttsx3)
+from simpleaudio import WaveObject
+from operator import itemgetter
+from langchain.chat_models import ChatOllama
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+
+# TODO: move defaults to config.yaml
+HOTKEY = "cmd+i"
+USER_INPUT_MESSAGE = f"\n('{HOTKEY}' to TALK or TYPE your query)"
+STT_MODEL = "tiny.en"
+CHAT_MODEL = "dolphin-phi:2.7b-v2.6-q6_K"
+SYSTEM_PROMPT="Your responses are concise. Do not give unwanted explanations. STICK TO WHAT IS REQUESTED. DO NOT MAKE THINGS UP. If there is anything you do not know, just reply: 'Sorry, I don't know'."
+SENTENCE_DELIMITERS = (".", "?", "!", ";", ":", " (", ")", "\n-")
+TTS_MODEL = "tts_models/en/vctk/vits"
+SPEAKER_ID = "p244"
+
+# TODO: improve logging (use a proper logger), remove hardcoded stdout prints.
+class Assistant:
+    def __init__(self, ollama_model=CHAT_MODEL, system_prompt=SYSTEM_PROMPT, delimiters=SENTENCE_DELIMITERS, stt_model=STT_MODEL, tts_model=TTS_MODEL, speaker_id=SPEAKER_ID, hotkey=HOTKEY, input_message=USER_INPUT_MESSAGE, auto_start=True):
+        self.q = queue.Queue()
+        self.device = 1 # TODO: allow choosing a different input device
+        self.channels = 1
+        self.ollama_model = ollama_model
+        self.system_prompt = system_prompt
+        self.delimiters = delimiters
+        self.speaker_id = speaker_id
+        self.hotkey = hotkey
+        self.input_message = input_message
+
+        # Load STT model
+        self.stt = whisper.load_model(stt_model)
+
+        # Load TTS model
+        self.tts = TTS(model_name=tts_model, progress_bar=False)
+
+        # Load LLM
+        self.llm = ChatOllama(model=ollama_model)
+
+        # Configure LLM memory
+        self.memory = ConversationBufferMemory(return_messages=True)
+
+        # Create LLM chain
+        self.chain = self._initialize_chain()
+
+        if auto_start:
+            self.start()
+
+
+    def _initialize_chain(self):
+        """Create LLM chat chain with memory"""
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", self.system_prompt),
+                MessagesPlaceholder(variable_name="history"),
+                ("human", "{input}"),
+            ]
+        )
+
+        return (
+            RunnablePassthrough.assign(
+                history=RunnableLambda(self.memory.load_memory_variables) | itemgetter("history")
+            )
+            | prompt
+            | self.llm
+            | self._split_sentences
+        )
+
+    def _split_sentences(self, chunks):
+        """Split/compile chunks into sentences"""
+        buffer = ""
+        for text in chunks:
+            if text.content.startswith(self.delimiters):
+                yield buffer + text.content[0]
+                buffer = text.content[1:]
+            else:
+                buffer += text.content
+        if buffer != "":
+            yield buffer
+
+
+    def start(self):
+        """Start assistant with default behavior"""
+        os.system('cls||clear')
+
+        keyboard.add_hotkey(self.hotkey, lambda: self.listen())
+
+        user_input_thread = threading.Thread(target=self._wait_input)
+        user_input_thread.start()
+        user_input_thread.join()
+
+        keyboard.wait()
+
+    def _wait_input(self):
+        """Process user input"""
+        print(self.input_message)
+        while True:
+            self.think(query=input())
+
+
+    def listen(self, is_standalone=False):
+        """Record microphone audio to a .wav file"""
+        try:
+            device_info = query_devices(self.device, 'input')
+            samplerate = int(device_info['default_samplerate'])
+
+            filename = tempfile.mktemp(suffix='.wav', dir='')
+            file = SoundFile(
+                filename,
+                mode='x',
+                samplerate=samplerate,
+                channels=self.channels
+            )
+
+            stream = InputStream(
+                samplerate=samplerate,
+                device=self.device,
+                channels=self.channels,
+                callback=self._input_stream_callback
+            )
+            stream.start()
+
+            print("\n🎙️  LISTENING...")
+            while keyboard.is_pressed('command') and keyboard.is_pressed('i'):
+                file.write(self.q.get())
+            
+            stream.close()
+            file.close()
+
+            if not is_standalone:
+                self.transcribe(audio_file=file.name)
+            
+            return file.name
+
+        except Exception as e:
+            raise type(e)(str(e))
+
+    def _input_stream_callback(self, indata, frames, time, status):
+        if status:
+            print(status, file=sys.stderr)
+        self.q.put(indata.copy())
+
+
+    def transcribe(self, audio_file, should_remove_audio=True, is_standalone=False):
+        """Transcribe an audio file"""
+        try:
+            print("📝 TRANSCRIBING...")
+            transcription = self.stt.transcribe(audio_file, fp16=False)
+            print(f"💬 >>>{transcription['text']}")
+
+            if should_remove_audio:
+                os.remove(audio_file)
+
+            if not is_standalone:
+                self.think(query=transcription['text'])
+            
+            return transcription['text']
+
+        except Exception as e:
+            raise type(e)(str(e))
+            
+
+    def think(self, query, is_standalone=False):
+        """Send a query to an LLM and stream response sentence-by-sentence"""
+        try:
+            print(f"\n🤔 THINKING...\n")
+            print("#" * 50)
+            input_data = {"input": query}
+            output = ""
+            for sentence in self.chain.stream(input_data):
+                output += sentence
+                if not is_standalone:
+                    self.synthesize(sentence)
+            
+            self.memory.save_context(input_data, {"output": output}) 
+            print("#" * 50)
+            print(f"\n💬 <<< {output}")
+            print(self.input_message)
+            return output
+        
+        except Exception as e:
+            raise type(e)(str(e))
+            
+
+    def synthesize(self, sentence, is_standalone=False):
+        """Synthesize text into an audio file"""
+        try:
+            print(f"\n🤖 SYNTHESIZING...")
+            filename = tempfile.mktemp(suffix='.wav', dir='')
+
+            self.tts.tts_to_file(
+                text=sentence,
+                speaker=self.speaker_id,
+                file_path=filename,
+                split_sentences=False
+            )
+
+            if not is_standalone:
+                self.speak(filename)
+                
+            return filename
+        
+        except Exception as e:
+            raise type(e)(str(e))
+        
+
+    def speak(self, audio_file, should_remove_audio=True):
+        """Play an audio file"""
+        try:
+            print(f"🔊 SPEAKING...\n")
+            wave_obj = WaveObject.from_wave_file(audio_file)
+            play_obj = wave_obj.play()
+            play_obj.wait_done()
+
+            if should_remove_audio:
+                os.remove(audio_file)
+        
+        except Exception as e:
+            raise type(e)(str(e))
--- main.py
+++ main.py
@@ -1,175 +1,43 @@
-import os
-import sys
-import queue
-import tempfile
-import whisper
-import keyboard
-from sounddevice import InputStream, query_devices
-from soundfile import SoundFile
-from TTS.api import TTS
-from simpleaudio import WaveObject
-from operator import itemgetter
-from langchain.chat_models import ChatOllama
-from langchain.memory import ConversationBufferMemory
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnableLambda, RunnablePassthrough
-
-
-# Configuration
-SENTENCE_DELIMITERS = (".", "?", "!", ";", ":", " (", ")", "\n-")
-STT_MODEL = "tiny.en"
-CHAT_MODEL = "dolphin-phi:2.7b-v2.6-q6_K"
-SYSTEM_PROMPT="You are a helpful assistant. Your responses to user queries are concise. Don't waste tokens on unwanted explanations or possible follow up questions. STICK TO WHAT THE USER REQUESTED. DO NOT MAKE THINGS UP. If there's anything you don't know, just reply: 'Sorry, I don't know'."
-TTS_MODEL = "tts_models/en/vctk/vits"
-SPEAKER_ID = "p244"
-
-# Load STT model
-stt_model = whisper.load_model(STT_MODEL)
-
-def output_chunker(chunks):
-    """Used during input streaming to chunk sentences"""
-    buffer = ""
-    for text in chunks:
-        if text.content.startswith(SENTENCE_DELIMITERS):
-            yield buffer + text.content[0]
-            buffer = text.content[1:]
-        else:
-            buffer += text.content
-    if buffer != "":
-        yield buffer
-
-# Configure/initialize LLM chain
-chat_model = ChatOllama(model=CHAT_MODEL)
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", SYSTEM_PROMPT),
-        MessagesPlaceholder(variable_name="history"),
-        ("human", "{input}"),
-    ]
-)
-memory = ConversationBufferMemory(return_messages=True)
-chain = (
-    RunnablePassthrough.assign(
-        history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
-    )
-    | prompt
-    | chat_model
-    | output_chunker
-)
-
-# Initialize TTS model
-tts = TTS(model_name=TTS_MODEL, progress_bar=False)
-
-# Lists the audio devices available
-# print(query_devices())
+from assistant import Assistant
 
 def main():
-    """Listen for keyboard events"""
     try:
-        os.system('cls||clear')
-        print(f"\n('cmd+i' to chat)")
-        keyboard.add_hotkey('cmd+i', lambda: record_mic())
-        keyboard.wait()
+        # Initialize and run an assistant out-of-the-box
+        Assistant()
 
-    except KeyboardInterrupt:
+        # # Example of standalone usage of assistant's functions
+
+        # # Initialize an assistant in manual mode
+        # assistant = Assistant(auto_start=False)
+
+        # query = "What are large language models good for?"
+        
+        # response = assistant.think(
+        #     query=query,
+        #     is_standalone=True
+        # )
+
+        # question_speech = assistant.synthesize(
+        #     sentence=response,
+        #     is_standalone=True
+        # )
+        
+        # question_transcription = assistant.transcribe(
+        #     audio_file=question_speech,
+        #     should_remove_audio=False,
+        #     is_standalone=True
+        # )
+        # print(f"> {question_transcription}")
+
+        # assistant.speak(audio_file=question_speech)
+
+        # # Start the assistant in auto-mode
+        # assistant.start()
+
+    except KeyboardInterrupt: 
         print("\n\n👋 BYE!")
     except Exception as e:
         raise type(e)(str(e))
-
-def record_mic():
-    """Start recording audio"""
-    q = queue.Queue()
-    device = 1 
-    channels = 1
-
-    def callback(indata, frames, time, status):
-        """This is called (from a separate thread) for each audio block"""
-        if status:
-            print(status, file=sys.stderr)
-        q.put(indata.copy())
-
-    try:
-        device_info = query_devices(device, 'input')
-        samplerate = int(device_info['default_samplerate'])
-
-        filename = tempfile.mktemp(suffix='.wav', dir='')
-        file = SoundFile(
-            filename,
-            mode='x',
-            samplerate=samplerate,
-            channels=channels
-        )
-
-        stream = InputStream(
-            samplerate=samplerate,
-            device=device,
-            channels=channels,
-            callback=callback
-        )
-        stream.start()
-
-        print("\n🎙️  LISTENING...")
-        while keyboard.is_pressed('command') and keyboard.is_pressed('i'):
-            file.write(q.get())
-
-    except Exception as e:
-        raise type(e)(str(e))
-    finally:
-        stream.close()
-        file.close()
-        transcribe_audio(file.name)
-        print(f"\n('cmd+i' to chat)")
-
-def transcribe_audio(filename):
-    """Transcribe the audio file"""
-    try:
-        print("📝 TRANSCRIBING...")
-        transcription = stt_model.transcribe(filename, fp16=False)
-        print(f"💬 >>>{transcription['text']}")
-        os.remove(filename)
-
-    except Exception as e:
-        raise type(e)(str(e))
-    finally:
-        query_llm(transcription['text'])
-
-def query_llm(query):
-    """Send transcription to llm and persist chat history"""
-    try:
-        print(f"\n🤔 THINKING...\n")
-        print("#" * 50)
-        input = {"input": query}
-        output = ""
-        for sentence in chain.stream(input):
-            output += sentence
-            synthesize_llm_response(sentence)
-
-    except Exception as e:
-        raise type(e)(str(e))
-    finally:
-        memory.save_context(input, {"output": output})
-        print("#" * 50)
-        print(f"\n💬 <<< {output}")
-
-def synthesize_llm_response(sentence):
-    """Synthesize the llm response and play audio"""
-    try:
-        print(f"\n🤖 SYNTHESIZING...")
-        tts.tts_to_file(
-            text=sentence,
-            speaker=SPEAKER_ID,
-            file_path="speech.wav",
-            split_sentences=False
-        )
-
-        print(f"🔊 SPEAKING...\n")
-        wave_obj = WaveObject.from_wave_file("speech.wav")
-        play_obj = wave_obj.play()
-        play_obj.wait_done()
-    except Exception as e:
-        raise type(e)(str(e))
-    finally:
-        os.remove("speech.wav")
 
 if __name__ == "__main__":
     main()
