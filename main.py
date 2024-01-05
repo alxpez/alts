926bcc674d2212977715521c31d73b7c0bb205ef
alxpez <alexperez.dev@gmail.com>
2024-01-05 21:09:53 +1300

    feat: queue speech and play in a separate thread: - other minor bug fixes: llm streaming and delimiters - make assistant methods responses consistent - clean prints from assistant api

--- main.py
+++ main.py
@@ -1,42 +1,11 @@
 from assistant import Assistant
 
 def main():
+    assistant = Assistant(False)
+    
     try:
-        # Initialize and run an assistant out-of-the-box
-        Assistant()
+        assistant.start()
 
-        # # Example of standalone usage of assistant's functions
-
-        # # Initialize an assistant in manual mode
-        # assistant = Assistant(auto_start=False)
-
-        # query = "What are large language models good for?"
-        
-        # response = assistant.think(
-        #     query=query,
-        #     is_standalone=True
-        # )
-
-        # response_speech = assistant.synthesize(
-        #     sentence=response,
-        #     is_standalone=True
-        # )
-        
-        # response_transcription = assistant.transcribe(
-        #     audio_file=response_speech,
-        #     should_remove_audio=False,
-        #     is_standalone=True
-        # )
-
-        # print(f"\n\n> {response_transcription}")
-
-        # assistant.speak(audio_file=response_speech)
-
-        # # Start the assistant in auto-mode
-        # assistant.start()
-
-    except KeyboardInterrupt: 
-        print("\n\n👋 BYE!")
     except Exception as e:
         raise type(e)(str(e))
 
